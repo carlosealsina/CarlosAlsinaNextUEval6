91ef464fb67ae67a766adb8e734b43c937073083
carlosealsina <47729495+carlosealsina@users.noreply.github.com>
2021-03-05 09:54:21 -0500

    Add files via upload
    index js v.5-3-21

--- js/index.js
+++ js/index.js
@@ -1,3 +1,81 @@
+//función para mostrar todos los datos
+$(document).ready(function(){
+
+  document.querySelector('#mostrarTodos').addEventListener('click', mostrarTodos);
+  function mostrarTodos(){
+    
+    $.ajax({
+      type:'GET',
+      url:'./buscador.php?opcion=1',
+      data:{},
+      success: function(res){
+        var datos = JSON.parse(res);
+        //console.log(datos);
+        let respuesta = document.querySelector('#respuesta');
+          respuesta.innerHTML = '';
+          for(let item of datos){
+            respuesta.innerHTML += '<div class="tituloContenido"><div class=" card horizontal"><div class="card-image"><img src="img/home.jpg"></div><div class="card-stacked"><div class="card-content"><p><b>Dirección:</b> '+item.Direccion+'<br><b>Ciudad: </b>'+item.Ciudad+'<br><b>Teléfono: </b>'+item.Telefono+'<br><b>Código Postal:</b> '+item.Codigo_Postal+'<br><b>Tipo:</b> '+item.Tipo+'<br><b>Precio:</b> <div class="precioTexto" style="margin-left:50px; margin-top:-27px;">'+item.Precio+'</div></p></div><div class="card-action"><a style="color:black" href="#"><b>VER MÁS</b></a></div></div></div></div>';
+          }
+      }
+    })
+
+  }
+  
+  // consulta y cargue de selectCiudad
+  $.ajax({
+    url:'./buscador.php?opcion=2',
+    type:'GET',
+    data:{},
+    success: function(res){
+      let datos=JSON.parse(res);
+      //console.log(datos);
+      for (var i=0; i<=100; i++) {
+        if(datos[i]!=undefined){
+          //console.log(datos[i]);
+          $("#selectCiudad").append("<option value="+datos[i]+">" + datos[i] + "</option>");
+          $('select').formSelect();
+        }
+      }
+    }
+  })
+
+  // consulta y cargue de selecTipo
+  $.ajax({
+    url:'./buscador.php?opcion=3',
+    type:'GET',
+    data:{},
+    success: function(res){
+      let datos=JSON.parse(res);
+      //console.log(datos);
+      for (var i=0; i<=100; i++) {
+        if(datos[i]!=undefined){
+          $("#selectTipo").append('<option value=' + datos[i] + '>' + datos[i] + '</option>');
+          $('select').formSelect();
+        }
+      }
+    }
+  })
+
+  $("#formulario").on('click', function(e){
+    e.preventDefault();
+    var precio = $("#rangoPrecio").data("ionRangeSlider");
+    var minimo = precio.result.from;
+    var maximo = precio.result.to;
+
+    $.ajax({
+      url:'./buscador.php?opcion=4',
+      type:'GET',
+      data:{minimo:minimo, maximo:maximo},
+      success:function(res){
+        console.log(res);
+      }
+    })
+
+  })
+
+})
+
+
 /*
   Creación de una función personalizada para jQuery que detecta cuando se detiene el scroll en la página
 */
@@ -36,17 +114,19 @@ function playVideoOnScroll(){
     .scroll((event)=>{
       var scrollActual = $(window).scrollTop();
       if (scrollActual > ultimoScroll){
-       //video.play();
+       video.play();
      } else {
         //this.rewind(1.0, video, intervalRewind);
-        //video.play();
+        video.play();
      }
      ultimoScroll = scrollActual;
     })
     .scrollEnd(()=>{
-      //video.pause();
+      video.pause();
     }, 10)
 }
 
 inicializarSlider();
-playVideoOnScroll();
+//playVideoOnScroll();
+
+
